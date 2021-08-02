a54e8cfb64bb751113ae2ce4eebef1eff7202ba9
賴紀廷 <you@example.com>
2021-08-02 21:04:52 +0800

    java

---
+++ spring/src/main/java/com/study/spring/Hello.java
@@ -0,0 +1,9 @@
+package com.study.spring;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.print("中文");
+		System.out.println("spring");
+	}
+}
